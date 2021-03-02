d2cdc7ff925e2d78e27482581b9493fdf0eaacfc
IbroCalculus <38360810+IbroCalculus@users.noreply.github.com>
2021-03-02 08:16:02 +0100

    Create lambda_expression.py

---
+++ lambda_expression.py
@@ -0,0 +1,9 @@
+
+#Lambda square of number
+square_of_number = lambda z: z**2
+print(F'SQUARE: {square_of_number(20)}')
+
+
+#Lambda quadratic equation: y = 3*(X**2) + 4*X + 5
+quad = lambda X: 3*(X**2) + 4*X + 5
+print(f'QUAD: {quad(3)}')
